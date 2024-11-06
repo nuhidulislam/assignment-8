eeacfaf64402dbf1eadfc1a96d371fcfcddd7b0c
nuhidulislam <nuhidulislam@gmail.com>
2024-11-06 20:31:04 +0600

    error page setup

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1 className="text-5xl font-bold">Error Page</h1>
+        <div className="flex flex-col justify-center items-center h-[500px]  space-y-2">
+            <h1 className="text-9xl font-bold">Oops!</h1>
+            <p className="text-6xl font-semibold pt-3">404- PAGE NOT FOUND</p>
+            <p className="text-3xl font-bold ">We are unable to find this page. </p>
+            <p className="text-3xl font-bold " >This page is temporarily unavailable. </p>
+            <div className="px-6 py-2 bg-green-300 rounded-xl text-2xl font-bold"><Link to='/'>Go To Home Page</Link></div>
         </div>
     );
 };
